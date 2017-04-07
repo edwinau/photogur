a7d6ee108f2bf2308a8d6ae91a2cd5b866d48794
EDWIN AU <edwinau@rogers.com>
2017-04-07 16:26:17 -0400

    complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,9 +8,6 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
-  def new
-  end
-
   def create
     render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
   end
@@ -36,23 +33,24 @@ class PicturesController < ApplicationController
     params.require(:picture).permit(:artist, :title, :url)
   end
 
-
   def edit
-      @picture = Picture.find(params[:id])
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
     end
+  end
 
-    def update
-      @picture = Picture.find(params[:id])
-
-      if @picture.update_attributes(picture_params)
-        redirect_to "/pictures/#{@picture.id}"
-      else
-        render :edit
-      end
-    end
-
-
-
-
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,6 @@ Rails.application.routes.draw do
 
   root 'pictures#index'
 
-
-  get 'pictures' => 'pictures#index'
-
-    post 'pictures' => 'pictures#create' # this is a new line of code
-    get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-patch 'pictures/:id' => "pictures#update"
-
+  resources :pictures
 
 end
